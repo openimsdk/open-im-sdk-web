7691009f6d90f149edbcc34a0ff1bc37cf920714
Polarish <72297937+AllianceTing@users.noreply.github.com>
2023-07-17 22:00:33 +0800

    Revert "feat: add friend/group api for miniprogram"

--- src/im/index.ts
+++ src/im/index.ts
@@ -1775,7 +1775,7 @@ export default class OpenIMSDK extends Emitter {
 
   private wsSend = (params: WsParams, resolve: (value: WsResponse | PromiseLike<WsResponse>) => void, reject: (reason?: any) => void) => {
     if (window?.navigator && !window.navigator.onLine) {
-      let errData: WsResponseForMiniProgram = {
+      let errData: WsResponse = {
         event: params.reqFuncName,
         errCode: 113,
         errMsg: "net work error",
